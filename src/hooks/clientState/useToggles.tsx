79211a01f6f6a51a1e773b0a32707b0cf3946540
Frank Nyambe <franknyambe213@gmail.com>
2026-04-28 23:59:55 +0200

    Copied toggleState

--- src/hooks/clientState/useToggles.tsx
+++ src/hooks/clientState/useToggles.tsx
@@ -3,6 +3,8 @@ import { create } from "zustand";
 interface TogglesState {
   isUserOpen: boolean;
   isContactOpen: boolean;
+  sidebarOpen: boolean;
+  toggleSidebarOpen: (newState: boolean) => void;
   toggleContactOpen: (newState: boolean) => void;
   toggleUserOpen: (newState: boolean) => void;
 }
@@ -10,6 +12,8 @@ interface TogglesState {
 export const useToggleState = create<TogglesState>((set) => ({
   isUserOpen: false,
   isContactOpen: false,
+  sidebarOpen: false,
   toggleUserOpen: (newState: boolean) => set({ isUserOpen: newState }),
-  toggleContactOpen: (newState: boolean) => set({isContactOpen: newState})
+  toggleContactOpen: (newState: boolean) => set({ isContactOpen: newState }),
+  toggleSidebarOpen: (newState: boolean) => set({ sidebarOpen: newState }),
 }));
