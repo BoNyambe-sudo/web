8e48f61be6a879aa1c971910f603306f87098490
Frank Nyambe <franknyambe213@gmail.com>
2026-06-18 11:58:47 +0200

    feat: add "Already up to date" toast notification for user and admin forms when no changes are made

--- src/pages/admin/AdminUsers.tsx
+++ src/pages/admin/AdminUsers.tsx
@@ -170,7 +170,6 @@ const AdminUsers = () => {
 
   function handleSaveUserChanges(event: React.SubmitEvent) {
     event.preventDefault();
-    console.log("role: ", formData.role, "id: ", formData.id);
     updateUser(
       { id: formData?.id as string, user: { role: formData.role } },
       {
